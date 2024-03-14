5dcc9d6808571018b09a6b5b9257ea95ed11f4f4
Muhammad Haikal Al Jufri <muhammad.haikal.tif23@polban.ac.id>
2024-03-14 10:56:07 +0700

    fungsi mencocokkan data mahasiswa dan dosen disatukan

--- Modul 1/main.cpp
+++ Modul 1/main.cpp
@@ -5,167 +5,61 @@
 using namespace std;
 
 // Format data dosen
-struct Dosen
-{
+struct Dosen {
     string nama;
     string NIP;
     string password;
 };
 
 // Format data mahasiswa
-struct Mahasiswa
-{
+struct Mahasiswa {
     string nama;
     string NIM;
     string password;
 };
 
-bool loginUser(struct user)
-{
-    string fileUser;
-    if (dosen)
-    {
-        fileUser = "data_dosen.txt" ifstream file(fileUser);
-        if (file.is_open())
-        {
-            string line;
-            while (getline(file, line))
-            {
-                if (line.find("NIP: " + NIP) != string::npos)
-                {
-                    getline(file, line); // Baca password
-                    if (line.find("Password: " + password) != string::npos)
-                    {
-                        file.close();
-                        return true;
-                    }
+// Fungsi untuk mencocokan data dengan jenis yang ditentukan (dosen atau mahasiswa)
+bool login(const string& id, const string& password, const string& filename, const string& id_label) {
+    ifstream file(filename);
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            if (line.find(id_label + id) != string::npos) {
+                getline(file, line); // Baca password
+                if (line.find("Password: " + password) != string::npos) {
+                    file.close();
+                    return true;
                 }
             }
-            file.close();
         }
-        else
-        {
-            fileUser = "data_mahasiswa.txt";
-            ifstream file(fileUser);
-            if (file.is_open())
-            {
-                string line;
-                while (getline(file, line))
-                {
-                    if (line.find("NIP: " + NIP) != string::npos)
-                    {
-                        getline(file, line); // Baca password
-                        if (line.find("Password: " + password) != string::npos)
-                        {
-                            file.close();
-                            return true;
-                        }
-                    }
-                }
-                file.close();
-            }
-            return false;
+        file.close();
+    }
+    return false;
+}
+
+// Fungsi utama
+int main() {
+    string role;
+    cout << "Anda akan registrasi sebagai (dosen/mahasiswa): ";
+    cin >> role;
+
+    if (role == "dosen" || role == "mahasiswa") {
+        string id, password;
+        string filename = (role == "dosen") ? "data_dosen.txt" : "data_mahasiswa.txt";
+        string id_label = (role == "dosen") ? "NIP: " : "NIM: ";
+        cout << "Masukkan " << ((role == "dosen") ? "NIP" : "NIM") << ": ";
+        cin >> id;
+        cout << "Masukkan password: ";
+        cin >> password;
+
+        if (login(id, password, filename, id_label)) {
+            cout << "Login berhasil" << endl;
+        } else {
+            cout << "Login gagal. " << ((role == "dosen") ? "NIP" : "NIM") << " atau password salah." << endl;
         }
+    } else {
+        cout << "Peran yang dimasukkan tidak valid." << endl;
     }
 
-    // Untuk mencocokan data dosen
-    bool loginDosen(const string &NIP, const string &password)
-    {
-        ifstream file("data_dosen.txt");
-        if (file.is_open())
-        {
-            string line;
-            while (getline(file, line))
-            {
-                if (line.find("NIP: " + NIP) != string::npos)
-                {
-                    getline(file, line); // Baca password
-                    if (line.find("Password: " + password) != string::npos)
-                    {
-                        file.close();
-                        return true;
-                    }
-                }
-            }
-            file.close();
-        }
-        return false;
-    }
-
-    // Untuk mencocokan data mahasiswa
-    bool loginMahasiswa(const string &NIM, const string &password)
-    {
-        ifstream file("data_mahasiswa.txt");
-        if (file.is_open())
-        {
-            string line;
-            while (getline(file, line))
-            {
-                if (line.find("NIM: " + NIM) != string::npos)
-                {
-                    getline(file, line); // Baca password
-                    if (line.find("Password: " + password) != string::npos)
-                    {
-                        file.close();
-                        return true;
-                    }
-                }
-            }
-            file.close();
-        }
-        return false;
-    }
-    // Sebelumnya udah punya 2 file teks yang berisi data dosen dan mahasiswa jadi dibaca disisni
-
-    // buat login bakal ditanya dulu akan login sebagai apa
-    int main()
-    {
-        string role;
-        cout << "Anda akan registrasi sebagai (dosen/mahasiswa): ";
-        cin >> role;
-
-        // ini kalau dosen
-        if (role == "dosen")
-        {
-            struct Dosen dosen;
-            // string NIP, password;
-            cout << "Masukkan NIP: ";
-            cin >> dosen.NIP;
-            cout << "Masukkan password: ";
-            cin >> dosen.password;
-
-            if (loginDosen(dosen))
-            {
-return dosen;            }
-            else
-            {
-                cout << "Login gagal. NIP atau passw";
-                }
-        
-
-            // ini kalau mahasiswa
-        }
-        else if (role == "mahasiswa")
-        {
-            string NIM, password;
-            cout << "Masukkan NIM: ";
-            cin >> NIM;
-            cout << "Masukkan password: ";
-            cin >> password;
-
-            if (loginMahasiswa(NIM, password))
-            {
-                cout << "Login berhasil" << endl;
-            }
-            else
-            {
-                cout << "Login gagal. NIM atau password salah." << endl;
-            }
-        }
-        else
-        {
-            cout << "Peran yang dimasukkan tidak valid." << endl; // ini kalau masukin selain dosen atau mahasiswa
-        }
-
-        return 0;
-    }
+    return 0;
+}
