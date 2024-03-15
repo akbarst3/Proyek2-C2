0d1b7631750ad2f02d93c690d9f9b139cd908f5c
Restu Akbar <restu.akbar.tif23@polban.ac.id>
2024-03-15 22:16:51 +0700

    modul dosen buat soal

---
+++ 231511088/231511088.cpp
@@ -0,0 +1,93 @@
+#include "231511088.h"
+
+void simpanKeFile(const ujian *soalBaru, string namaFile, int jumlahSoal)
+{
+    ofstream file("../assets/folder-soal/" + namaFile + ".txt");
+    if (file.is_open())
+    {
+        for (int i = 0; i < jumlahSoal; i++)
+        {
+            file << i + 1 << ". " << soalBaru[i].pertanyaan << endl;
+            for (int j = 0; j < 4; j++)
+            {
+                file << " " << char('A' + j) << ". " << soalBaru[i].jawaban[j] << " ";
+            }
+            file << endl;
+        }
+        file.close();
+        cout << "Soal telah disimpan ke dalam folder-soal dengan nama file '" << namaFile << ".txt'" << endl;
+    }
+    else
+    {
+        cout << "Gagal membuka file untuk penyimpanan." << endl;
+    }
+}
+
+
+string toUpperCase(string str)
+{
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+    return str;
+}
+
+void buatSoal(string mataUjian, int jumlahSoal, ujian *soalBaru)
+{
+    system("cls");
+    cout << "-------------------------------------------------\n";
+    cout << "     UJIAN " << toUpperCase(mataUjian) << '\n';
+    cout << "-------------------------------------------------\n";
+    cin.ignore();
+
+    for (int i = 0; i < jumlahSoal; i++)
+    {
+        cout << i + 1 << ". ";
+        getline(cin, soalBaru[i].pertanyaan);
+        for (int j = 0; j < 4; j++)
+        {
+            cout << " " << char('a' + j) << ". ";
+            getline(cin, soalBaru[i].jawaban[j]);
+        }
+        cout << endl;
+    }
+    simpanKeFile(soalBaru, mataUjian, jumlahSoal);
+}
+
+string dashboard(user dosen)
+{
+    char answer;
+
+    string mataUjian; // Pindahkan deklarasi di sini
+    int jumlahSoal;   // Pindahkan deklarasi di sini
+    ujian *soalBaru;
+
+    // Tampilkan dashboard
+    cout << "------------------------------\n";
+    cout << "|                            |\n";
+    cout << "|       DASHBOARD DOSEN      |\n";
+    cout << "|                            |\n";
+    cout << "------------------------------\n";
+    cout << "Selamat datang di dasbor dosen" << endl;
+    cout << dosen.nama << endl;
+    cout << dosen.id << endl;
+    cout << "------------------------------" << endl;
+    cout << "(1) Buat soal" << endl;
+    cout << "(2) Logout" << endl;
+    cin >> answer;
+
+    switch (answer)
+    {
+    case '1':
+        cout << "Nama mata ujian: ";
+        cin >> mataUjian;
+        cout << "Jumlah soal: ";
+        cin >> jumlahSoal;
+        soalBaru = new ujian[jumlahSoal];
+        buatSoal(mataUjian, jumlahSoal, soalBaru);
+        break;
+    case '2':
+        return "Logout";
+    default:
+        cout << "---Karakter yang diinputkan tidak valid---" << endl;
+        return dashboard(dosen); // Kembalikan nilai dari default case
+    }
+}
---
+++ 231511088/231511088.h
@@ -0,0 +1,25 @@
+#ifndef HEAD088_H
+#define HEAD088_H
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+struct ujian
+{
+    string pertanyaan;
+    string jawaban[4];
+};
+
+string dashboard(user dosen);
+void buatSoal(string mataUjian, int jumlahSoal, ujian *soalBaru);
+string toUpperCase(string str);
+void simpanKeFile(const ujian *soalBaru, string namaFile, int jumlahSoal)
+
+
+
+
+
+#endif
