ffecb79aad7f396247193dcba0e57e1e0323ce50
Isyana Putri Indriana <isyanap@gmail.com>
2024-03-17 14:04:54 +0700

    enkrip playfair

--- 231511078/231511078.cpp
+++ 231511078/231511078.cpp
@@ -1,91 +1,67 @@
 #include "231511078.h"
+const string list1 = "abcdefghiklmnopqrstuvwxyz";
 
-PlayfairCipher::PlayfairCipher(const std::string& key) : key(key) {
-    prepareKey();
-    prepareMatrix();
+void removeSpaces(string& str) {
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
 }
 
-void PlayfairCipher::prepareKey() { // Menyalin kunci ke variabel sementara dan konversi menjadi huruf kapital
-    std::string tempKey = key;
-    std::transform(tempKey.begin(), tempKey.end(), tempKey.begin(), ::toupper); 
-    tempKey.erase(std::remove(tempKey.begin(), tempKey.end(), ' '), tempKey.end()); // Hapus spasi dari kunci
-    std::string::iterator end_pos = std::unique(tempKey.begin(), tempKey.end()); // Hapus karakter duplikat dari kunci
-    tempKey.erase(end_pos, tempKey.end()); // Isi karakter-karakter tersisa dari kunci (tidak termasuk 'J')
-    for (char ch = 'A'; ch <= 'Z'; ++ch) { 
-        if (ch == 'J') continue; 
-        if (tempKey.find(ch) == std::string::npos) {
-            tempKey.push_back(ch);
-        }
-    }
-    if (tempKey.length() < 25) { // Jika panjang kunci kurang dari 25, isi karakter-karakter tersisa
-        for (char ch = 'A'; ch <= 'Z' && tempKey.length() < 25; ++ch) {
-            if (ch == 'J') continue;
-            if (tempKey.find(ch) == std::string::npos) {
-                tempKey.push_back(ch);
-            }
-        }
-    }
-    this->key = tempKey.substr(0, 25); // Perbarui variabel anggota key dengan kunci yang telah diproses (25 karakter pertama)
-}
-
-void PlayfairCipher::prepareMatrix() {
-    int k = 0;  // Isi matriks 5x5 dengan karakter-karakter dari kunci
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            matrix[i][j] = key[k++];
+void toLowerCase(string& str) {
+    for (char& ch : str) {
+        if (isupper(ch)) {
+            ch = tolower(ch);
         }
     }
 }
 
-void PlayfairCipher::getCoordinates(char ch, int& row, int& col) {
-    if (ch == 'J') ch = 'I'; // Temukan koordinat baris dan kolom dari sebuah karakter dalam matriks
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            if (matrix[i][j] == ch) {
-                row = i;
-                col = j;
-                return;
-            }
+void fillDiagraph(string& str) {
+    for (size_t i = 0; i < str.length(); i += 2) {
+        if (i + 1 == str.length() || str[i] == str[i + 1]) {
+            str.insert(i + 1, "z");
         }
     }
 }
 
-std::string PlayfairCipher::fixText(const std::string& text) {
-    std::string result = text; // Konversi teks menjadi huruf kapital dan ganti karakter non-alfabet dengan spasi
-    std::transform(result.begin(), result.end(), result.begin(), ::toupper);
-    for (char& ch : result) {
-        if (!isalpha(ch)) {
-            ch = ' ';
-        } else if (ch == 'J') {
-            ch = 'I';
+void generateKeyTable(const string& key, string& table) {
+    table = key;
+    for (char ch : list1) {
+        if (table.find(ch) == string::npos) {
+            table += ch;
         }
     }
-    result.erase(std::remove(result.begin(), result.end(), ' '), result.end()); // Hapus spasi dan tambahkan 'X' jika panjang teks ganjil
-    if (result.length() % 2 != 0) {
-        result.push_back('X');
-    }
-    return result;
 }
 
-std::string PlayfairCipher::encrypt(const std::string& plaintext) {
-    std::string fixedText = fixText(plaintext);
-    std::string ciphertext = ""; // Enkripsi teks yang sudah diperbaiki menggunakan algoritma sandi Playfair
-    for (size_t i = 0; i < fixedText.length(); i += 2) {
-        char ch1 = fixedText[i];
-        char ch2 = fixedText[i + 1];
-        int row1, col1, row2, col2;
-        getCoordinates(ch1, row1, col1);
-        getCoordinates(ch2, row2, col2);
-        if (row1 == row2) { 
-            ciphertext += matrix[row1][(col1 + 1) % 5];
-            ciphertext += matrix[row2][(col2 + 1) % 5];
-        } else if (col1 == col2) { 
-            ciphertext += matrix[(row1 + 1) % 5][col1];
-            ciphertext += matrix[(row2 + 1) % 5][col2];
-        } else { 
-            ciphertext += matrix[row1][col2];
-            ciphertext += matrix[row2][col1];
+void encryptByPlayfairCipher(const string& table, const string& plainText, string& cipherText) {
+    cipherText = "";
+    for (size_t i = 0; i < plainText.length(); i += 2) {
+        char ch1 = plainText[i];
+        char ch2 = (i + 1 < plainText.length()) ? plainText[i + 1] : 'x';
+
+        size_t pos1 = table.find(ch1);
+        size_t pos2 = table.find(ch2);
+
+        size_t row1 = pos1 / 5;
+        size_t col1 = pos1 % 5;
+        size_t row2 = pos2 / 5;
+        size_t col2 = pos2 % 5;
+
+        if (row1 == row2) {
+            cipherText += table[row1 * 5 + (col1 + 1) % 5];
+            cipherText += table[row2 * 5 + (col2 + 1) % 5];
+        } else if (col1 == col2) {
+            cipherText += table[((row1 + 1) % 5) * 5 + col1];
+            cipherText += table[((row2 + 1) % 5) * 5 + col2];
+        } else {
+            cipherText += table[row1 * 5 + col2];
+            cipherText += table[row2 * 5 + col1];
         }
     }
-    return ciphertext;
+}
+
+void printMatrix(const string& table) {
+    for (size_t i = 0; i < table.length(); ++i) {
+        cout << table[i] << " ";
+        if ((i + 1) % 5 == 0) {
+            cout << endl;
+        }
+    }
 }
--- 231511078/231511078.h
+++ 231511078/231511078.h
@@ -2,22 +2,16 @@
 #define HEAD078_H
 
 #include <string>
-#include <vector>
-#include <algorithm>
+#include <iostream>
+#include<algorithm>
+using namespace std;
 
-class PlayfairCipher {
-private:
-    std::string key;
-    char matrix[5][5];
 
-    void prepareKey();
-    void prepareMatrix();
-    void getCoordinates(char ch, int& row, int& col);
-    std::string fixText(const std::string& text);
+void removeSpaces(std::string& str);
+void toLowerCase(std::string& str);
+void fillDiagraph(std::string& str);
+void generateKeyTable(const std::string& key, std::string& table);
+void encryptByPlayfairCipher(const std::string& table, const std::string& plainText, std::string& cipherText);
+void printMatrix(const std::string& table);
 
-public:
-    PlayfairCipher(const std::string& key);
-    std::string encrypt(const std::string& plaintext);
-};
-
-#endif  
+#endif /
