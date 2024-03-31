5c38d022cc26af623c0721f55bc1c1681111da58
Jufri23 <muhammad.haikal.tif23@polba.ac.id>
2024-03-31 20:46:22 +0700

    Modul nilai masuk modul nim.h

--- 231511085/231511085.cpp
+++ 231511085/231511085.cpp
@@ -79,3 +79,60 @@ user loginDosen()
     user userLogged = login(nip, password, users);
     return userLogged;
 }
+
+Grader::Grader(const std::string& kunci_file, const std::string& jawaban_file) : poin(0) {
+    bacaKunciJawaban(kunci_file);
+    bacaJawabanPengguna(jawaban_file);
+}
+
+void Grader::runGrader() {
+    periksaJawaban();
+    tampilkanPoin();
+}
+
+void Grader::bacaKunciJawaban(const std::string& kunci_file) {
+    std::ifstream kunciFile(kunci_file);
+
+    if (kunciFile.is_open()) {
+        std::string jawaban;
+        while (getline(kunciFile, jawaban)) {
+            kunci_jawaban.push_back(jawaban);
+        }
+        kunciFile.close();
+    } else {
+        std::cout << "Gagal membuka file kunci jawaban." << std::endl;
+    }
+}
+
+void Grader::bacaJawabanPengguna(const std::string& jawaban_file) {
+    std::ifstream jawabanFile(jawaban_file);
+
+    if (jawabanFile.is_open()) {
+        std::string jawaban;
+        while (getline(jawabanFile, jawaban)) {
+            jawaban_pengguna.push_back(jawaban);
+        }
+        jawabanFile.close();
+    } else {
+        std::cout << "Gagal membuka file jawaban pengguna." << std::endl;
+    }
+}
+
+void Grader::periksaJawaban() {
+    if (kunci_jawaban.size() == jawaban_pengguna.size()) {
+        for (size_t i = 0; i < kunci_jawaban.size(); ++i) {
+            if (kunci_jawaban[i] == jawaban_pengguna[i]) {
+                poin += 10;
+                std::cout << "Jawaban untuk soal " << (i+1) << " benar!" << std::endl;
+            } else {
+                std::cout << "Jawaban untuk soal " << (i+1) << " salah!" << std::endl;
+            }
+        }
+    } else {
+        std::cout << "Jumlah soal dalam kunci jawaban dan jawaban pengguna tidak sama." << std::endl;
+    }
+}
+
+void Grader::tampilkanPoin() {
+    std::cout << "Total poin: " << poin << std::endl;
+}
--- 231511085/231511085.h
+++ 231511085/231511085.h
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string>
+#include <vector>
 #include "../user.h"
 
 user login(const string &id, const string &password, const string &user);
 user loginMhs();
 user loginDosen();
+class Grader {
+public:
+    Grader(const std::string& kunci_file, const std::string& jawaban_file);
+    void runGrader();
+private:
+    std::vector<std::string> kunci_jawaban;
+    std::vector<std::string> jawaban_pengguna;
+    int poin;
+
+    void bacaKunciJawaban(const std::string& kunci_file);
+    void bacaJawabanPengguna(const std::string& jawaban_file);
+    void periksaJawaban();
+    void tampilkanPoin();
+};
