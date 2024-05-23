90450f524fcee2cb7b12deb3b4786393a38d3198
Isyana Putri Indriana <isyanap@gmail.com>
2024-05-23 19:43:09 +0700

    menghapus modul printMatrix

--- 231511078/231511078.cpp
+++ 231511078/231511078.cpp
@@ -131,19 +131,6 @@ void encryptByPlayfairCipher(MatrixNode* table, jawaban* plainText, jawaban*& ci
     }
 }
 
-void printMatrix(MatrixNode* table) {
-    MatrixNode* temp = table;
-    int index = 0;
-    while (temp) {
-        cout << temp->data << ' ';
-        index++;
-        if (index % 5 == 0) {
-            cout << endl;
-        }
-        temp = temp->next;
-    }
-}
-
 jawaban* PlayfairCipher(jawaban* plaintext, jawaban* key) {
     fillDiagraph(plaintext);
 
